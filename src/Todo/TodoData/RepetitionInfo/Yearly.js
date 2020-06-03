8254fba4a11fb25848546de04d0ec9165aefe0bf
Wellvergton Marques <wmc.jp@outlook.com>
2020-06-03 04:47:12 -0300

    Refactoring for proper displey date

--- src/Todo/TodoData/RepetitionInfo/Yearly.js
+++ src/Todo/TodoData/RepetitionInfo/Yearly.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Yearly(props) {
   let date = new Date();
-  date.setDate(props.date.day);
   date.setMonth(props.date.month);
+  date.setDate(props.date.day);
   date = date.toLocaleDateString("en-US", { day: "2-digit", month: "long" });
 
   return (
