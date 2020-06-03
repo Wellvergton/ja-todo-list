1a2e930c3ee929ccfff6280ed8d30756b2afc7c5
Wellvergton Marques <wmc.jp@outlook.com>
2020-06-03 04:48:42 -0300

    Proper status for daily, monthly, yearly, someday

--- src/setProperStatus.js
+++ src/setProperStatus.js
@@ -1,29 +1,120 @@
-const today = new Date();
+const currentDate = new Date();
+const currentDay = currentDate.getDay();
+const currentMonth = currentDate.getMonth();
+const currentYear = currentDate.getFullYear();
+const currentTime = new Date(currentYear, currentMonth, currentDay).getTime();
 
 const functions = {
   daily(data) {
-    if (
-      data.date === "" ||
-      (data.date < today.getDay() && data.status === "concluded")
-    ) {
-      data.date = today.getDay();
+    const dataBasedDate = new Date(
+      currentYear,
+      currentMonth,
+      data.date.day
+    ).getTime();
+
+    if (dataBasedDate === currentTime && data.status !== "concluded") {
       data.status = "today";
+      return data;
     }
 
-    if (data.date === today.getDay() && data.status === "concluded") {
+    if (dataBasedDate < currentTime && data.status === "concluded") {
+      data.date.day += 1;
+      data.status = "today";
+      return data;
+    }
+
+    if (dataBasedDate === currentTime && data.status === "concluded") {
       data.status = "concluded";
+      return data;
     }
 
-    if (data.date < today.getDay() && data.status !== "concluded") {
+    if (dataBasedDate < currentTime && data.status !== "concluded") {
       data.status = "delayed";
+      return data;
+    }
+  },
+
+  monthly(data) {
+    const dataBasedDate = new Date(
+      currentYear,
+      data.date.month,
+      data.date.day
+    ).getTime();
+
+    if (dataBasedDate === currentTime && data.status !== "concluded") {
+      data.status = "today";
+      return data;
+    }
+
+    if (dataBasedDate < currentTime && data.status === "concluded") {
+      data.date.month = currentMonth + 1;
+      data.status = "pending";
+      return data;
+    }
+
+    if (dataBasedDate >= currentTime && data.status === "concluded") {
+      data.status = "concluded";
+      return data;
+    }
+
+    if (dataBasedDate > currentTime) {
+      data.status = "pending";
+      return data;
+    }
+
+    if (dataBasedDate < currentTime && data.status !== "concluded") {
+      data.status = "delayed";
+      return data;
+    }
+  },
+
+  yearly(data) {
+    const dataBasedDate = new Date(
+      data.date.year,
+      data.date.month,
+      data.date.day
+    ).getTime();
+
+    if (dataBasedDate === currentTime && data.status !== "concluded") {
+      data.status = "today";
+      return data;
+    }
+
+    if (dataBasedDate < currentTime && data.status === "concluded") {
+      data.date.year = currentYear + 1;
+      data.status = "pending";
+      return data;
+    }
+
+    if (dataBasedDate >= currentTime && data.status === "concluded") {
+      data.status = "concluded";
+      return data;
+    }
+
+    if (dataBasedDate > currentTime) {
+      data.status = "pending";
+      return data;
+    }
+
+    if (dataBasedDate < currentTime && data.status !== "concluded") {
+      data.status = "delayed";
+      return data;
+    }
+  },
+
+  someday(data) {
+    if (data.status === "concluded") {
+      data.status = "concluded";
+    } else {
+      data.status = "someday";
     }
 
     return data;
-  },
+  }
 };
 
 function setProperStatus(data) {
-  if (functions[data.type]) {
+  if (data.status !== "deleted" && functions[data.type]) {
     return functions[data.type](data);
   }
 
