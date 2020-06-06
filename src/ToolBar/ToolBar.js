5d29135a00d9ba3ac013be2807499b576641d992
Wellvergton Marques <wmc.jp@outlook.com>
2020-06-06 18:57:32 -0300

    Converted toolbar to react-bootstrap

--- src/ToolBar/ToolBar.js
+++ src/ToolBar/ToolBar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./ToolBar.scss";
-import Octicon, { ThreeBars, Plus, X } from "@primer/octicons-react";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Octicon, { ListUnordered, Plus, X } from "@primer/octicons-react";
 
 class ToolBar extends React.Component {
   constructor(props) {
@@ -8,69 +11,68 @@ class ToolBar extends React.Component {
     this.state = {
       menuIsVisible: false,
     };
-    this.ul = React.createRef();
+    this.nav = React.createRef();
     this.showHideMenu = this.showHideMenu.bind(this);
-    this.setUlStyle = this.setUlStyle.bind(this);
+    this.setNavStyle = this.setNavStyle.bind(this);
   }
 
   showHideMenu() {
     this.setState({ menuIsVisible: !this.state.menuIsVisible });
   }
 
-  setUlStyle() {
-    this.ul.current.style.maxHeight = this.state.menuIsVisible
-      ? `${this.ul.current.scrollHeight}px`
-      : "";
-    this.ul.current.style.margin = this.state.menuIsVisible ? "" : "0 auto";
+  setNavStyle() {
+    this.nav.current.style.maxHeight = this.state.menuIsVisible
+      ? `${this.nav.current.scrollHeight}px`
+      : "0px";
+    this.nav.current.style.margin = this.state.menuIsVisible ? "" : "0 auto";
   }
 
   componentDidMount() {
-    this.setUlStyle();
+    this.setNavStyle();
   }
 
   componentDidUpdate() {
-    this.setUlStyle();
+    this.setNavStyle();
   }
 
   render() {
     const contexts = this.props.contexts.map((context, index) => {
       return (
-        <li
-          className={`nav-item my-2 text-capitalize ${
+        <Nav.Item
+          className={`my-2 text-capitalize ${
             context === this.props.contextName ? "d-none" : ""
           }`}
           onClick={this.props.changeContext}
           key={context + index}
         >
           {context}
-        </li>
+        </Nav.Item>
       );
     });
 
     return (
-      <nav className="navbar navbar-expand-md navbar-light bg-light">
-        <button className="btn btn-light p-0" onClick={this.showHideMenu}>
+      <Navbar bg="light" expand="md">
+        <Button variant="light" onClick={this.showHideMenu}>
           <Octicon
-            icon={this.state.menuIsVisible ? X : ThreeBars}
+            icon={this.state.menuIsVisible ? X : ListUnordered}
             size="medium"
           />
-        </button>
-        <span className="text-capitalize">{this.props.contextName}</span>
-        <button className="btn btn-light p-0" onClick={this.props.addTodo}>
+        </Button>
+        <Navbar.Text className="text-capitalize text-body">
+          {this.props.contextName}
+        </Navbar.Text>
+        <Button variant="light" onClick={this.props.addTodo}>
           <Octicon icon={Plus} size="medium" />
-        </button>
-        <ul ref={this.ul} id="navbar-list" className="navbar-nav w-100">
+        </Button>
+        <Nav ref={this.nav} className="w-100">
           {contexts}
-          <li className="nav-item my-0">
-            <button
-              className="btn btn-info btn-block"
-              onClick={this.props.onCreateContext}
-            >
+          <Nav.Item className="my-0">
+            <Button variant="info" block onClick={this.props.onCreateContext}>
               <span className="font-weight-bold">Create a context</span>
-            </button>
-          </li>
-        </ul>
-      </nav>
+            </Button>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
     );
   }
 }
