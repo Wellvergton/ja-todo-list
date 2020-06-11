9c4e4df57d76b44e78ef15b56392395b5be03ada
Wellvergton Marques <wmc.jp@outlook.com>
2020-06-11 02:07:02 -0300

    Deleted option is always the last

--- src/ToolBar/ToolBar.js
+++ src/ToolBar/ToolBar.js
@@ -60,40 +60,41 @@ class ToolBar extends React.Component {
   }
 
   render() {
-    const contexts = this.props.contexts.map((context, index) => {
-      return (
-        <Nav.Item
-          className={`my-2 text-capitalize ${
-            context === this.props.contextName ? "d-none" : ""
-          }`}
-          onClick={this.props.changeContext}
-          key={context + index}
-        >
-          {context}
-        </Nav.Item>
-      );
-    });
+    const contexts = this.props.contexts
+      .sort()
+      .filter((context) => context !== "deleted")
+      .map((context, index) => {
+        return (
+          <Nav.Item
+            className={`my-2 text-capitalize ${
+              context === this.props.contextName ? "d-none" : ""
+            }`}
+            onClick={this.props.changeContext}
+            key={context + index}
+          >
+            {context}
+          </Nav.Item>
+        );
+      });
 
     return (
       <>
-        {
-          this.state.showCreateContextModal &&
+        {this.state.showCreateContextModal && (
           <CreateContextModal
             show={this.state.showCreateContextModal}
             contexts={this.props.contexts}
             onClose={this.showHideContextModal}
             onSave={this.handleSave}
           />
-        }
-        {
-          this.state.showCreateTodoModal &&
+        )}
+        {this.state.showCreateTodoModal && (
           <CreateTodoModal
             show={this.state.showCreateTodoModal}
             contexts={this.props.contexts}
             onClose={this.showHideTodoModal}
             onSave={this.props.addTodo}
           />
-        }
+        )}
         <Navbar bg="light" expand="md">
           <Button variant="light" onClick={this.showHideMenu}>
             <Octicon
@@ -104,16 +105,25 @@ class ToolBar extends React.Component {
           <Navbar.Text className="text-capitalize text-body">
             {this.props.contextName}
           </Navbar.Text>
-          <Button
-            variant="light"
-            onClick={this.showHideTodoModal}
-          >
+          <Button variant="light" onClick={this.showHideTodoModal}>
             <Octicon icon={Plus} size="medium" />
           </Button>
           <Nav ref={this.nav} className="w-100">
             {contexts}
+            <Nav.Item
+              className={`my-2 text-capitalize ${
+                this.props.contextName === "deleted" ? "d-none" : ""
+              }`}
+              onClick={this.props.changeContext}
+            >
+              deleted
+            </Nav.Item>
             <Nav.Item className="my-0">
-              <Button variant="info" block onClick={this.showHideContextModal}>
+              <Button
+                variant="outline-info"
+                block
+                onClick={this.showHideContextModal}
+              >
                 <span className="font-weight-bold">Create a context</span>
               </Button>
             </Nav.Item>
