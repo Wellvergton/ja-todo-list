757291a164482b43c48900155f3fa8009762bf55
Wellvergton Marques <wmc.jp@outlook.com>
2020-06-14 21:04:23 -0300

    Issue #15

--- src/Todo/TodoData/RepetitionInfo/Yearly.js
+++ src/Todo/TodoData/RepetitionInfo/Yearly.js
@@ -1,14 +1,24 @@
 import React from "react";
 
 function Yearly(props) {
+  console.log(props.date);
   let date = new Date();
+  const baseYear =
+    props.date.month > date.getMonth()
+      ? date.getFullYear()
+      : date.getFullYear() + 1;
+  const isLeapYear =
+    (baseYear % 4 === 0 && baseYear % 100 !== 0) || baseYear % 400 === 0;
+
   date.setMonth(props.date.month);
-  date.setDate(props.date.day);
+  if (!isLeapYear && props.date.month === 1 && props.date.day === 29) {
+    date.setDate(28);
+  } else {
+    date.setDate(props.date.day);
+  }
   date = date.toLocaleDateString("en-US", { day: "2-digit", month: "long" });
 
-  return (
-    <p className="font-weight-bold">{date}</p>
-  );
+  return <p className="font-weight-bold">{date}</p>;
 }
 
 export default Yearly;
